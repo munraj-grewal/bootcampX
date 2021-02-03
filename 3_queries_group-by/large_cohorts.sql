5d58d7f16337258c8c4619a2e957dcb8324d1ded
munraj <munrajgrewal12@gmail.com>
2021-02-03 19:54:12 +0000

    group by queries

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name AS cohort_name, count(students.cohort_id) AS student_count
+FROM cohorts
+JOIN students ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.cohort_id) >= 18;
